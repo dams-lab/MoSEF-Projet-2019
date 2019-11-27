866d9057c6fff11ae52d1abca3fe1cb6497cc185
dams-lab <da.raimond@gmail.com>
2019-11-27 20:44:47 +0100

    correction de petites erreurs et amélioration du README

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -3,12 +3,15 @@
 echo "ce fichier a été executé le $(date)" 
 echo "Bienvenue Monsieur dans ce projet linux, quel répertoire cherchez vous?"
 read -p 'tapez le répertoire que dans lequel vous souhaitez entrer: ' doss
-
+find $doss  
 less  "$doss"
 
-echo "$doss"
-read -p 'quels fichiers chezrchez vous? le paramètre 1 est' fich
-read -p 'quels contenus souhaitez-vous recherchez dedans? le paramètre 2 est:' detail
-sudo updatedb
-locate $doss/$fich | grep -i $detail
 
+read -p 'quels fichiers cherchez vous dans le dossier précédent?: ' fich
+read -p 'Choississez un mot ou une phrase, nous vous montrerons les fichiers dans le dossier séléctionné en haut le possédant! : ' detail
+sudo updatedb
+echo " voilà le fichier rechercher, ou l'ensemble si on utilise des métaparamètres tels que * et ?"
+locate $doss/$fich
+echo "Mnt voilà les fichiers possédant les caractères que vous avez spécifié plus haut"
+grep -il $detail $doss/$fich
+ 
